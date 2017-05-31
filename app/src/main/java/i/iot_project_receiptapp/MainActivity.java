440e9c63c36e7409c1b39342e8b8e61b0661f482
103000004 <alice103000004@gmail.com>
2017-05-31 19:57:49 +0800

    Version 5 with new Android Studio IDE 2.3.2

--- app/src/main/java/i/iot_project_receiptapp/MainActivity.java
+++ app/src/main/java/i/iot_project_receiptapp/MainActivity.java
@@ -162,6 +162,8 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
+            Intent i = new Intent(MainActivity.this, BluetoothActivity.class);
+            startActivity(i);
 
         }
 
